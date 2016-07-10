1bc0ee0b39481beec6f08622486c303cf44baa77
DrummerHead <drummerhead@gmail.com>
2016-07-10 14:30:20 -0400

    proper layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,43 @@
-var videos = window.videos;
+(function(window, document){
+  'use strict';
 
-var pad = function(num){
-  return num < 10 ? '0' + num : num;
-}
+  var videos = window.videos;
 
-var durationToString = function(duration){
-  var hours = duration.hours > 0 ? `${duration.hours}:` : '';
-  return `${hours}${pad(duration.minutes)}:${pad(duration.seconds)}`;
-}
+  var pad = function(num){
+    return num < 10 ? '0' + num : num;
+  };
 
-var videosHtml = videos.map((video) => {
-  console.log(video.views)
-  return `
-  <li>
-    <a href='https://www.youtube.com/watch?v=${video.id}'>
-      <img src='http://img.youtube.com/vi/${video.id}/hqdefault.jpg' alt='${video.title}'>
-      <h2>${video.title}</h2>
-      <ul classs='metadata'>
-        <li>${durationToString(video.duration)}</li>
-        <li>${video.views} views</li>
-        <li>${video.keyword}</li>
-      </ul>
-      <strong>${video.views}</strong>
-    </a>
-  </li>
-  `
-}).reduce((prev, curr) => prev + curr);
-console.log(videosHtml)
+  var stringToHue = function(string){
+    return string.split('')
+      .map((letter) => letter.charCodeAt())
+      .reduce((prev, curr) => prev + curr) % 360;
+  };
 
-document.getElementById('videos').insertAdjacentHTML('beforeend', videosHtml);
+  var durationToString = function(duration){
+    var hours = duration.hours > 0 ? `${duration.hours}:` : '';
+    return `${hours}${pad(duration.minutes)}:${pad(duration.seconds)}`;
+  };
+
+  var videosHtml = videos.map((video) => {
+    return `
+    <li class='video'>
+      <a class='video-thumb' href='https://www.youtube.com/watch?v=${video.id}'>
+        <img src='http://img.youtube.com/vi/${video.id}/hqdefault.jpg' alt='${video.title}'>
+        <ul class='metadata'>
+          <li class='duration'>${durationToString(video.duration)}</li>
+          <li class='views'>${video.views.toLocaleString()} views</li>
+          <li class='keyword' style='background-color: hsla(${stringToHue(video.keyword)}, 100%, 50%, .5)'>${video.keyword}</li>
+        </ul>
+      </a>
+      <h2 class='title'>
+        <a href='https://www.youtube.com/watch?v=${video.id}'>
+          ${video.title}
+        </a>
+      </h2>
+    </li>
+    `;
+  }).reduce((prev, curr) => prev + curr);
+
+  document.getElementById('videos').insertAdjacentHTML('beforeend', videosHtml);
+
+})(window, document);
